e9bb4dc041480d0419a8686a5f28e611e4754755
bartswierz <bartoszswierzynski7@gmail.com>
2023-11-09 00:48:49 -0600

    added login form, edit form,auth, config files for login authentication work in progress

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,23 @@ import type { NextAuthConfig } from "next-auth";
 
 //You can use the pages option to specify the route for custom sign-in, sign-out, and error pages. It is not required, but if you don't provide it, NextAuth.js will use its default sign-in, sign-out, and error pages. By adding signIn: '/login' into our pages option, the user will be redirected to our custom login page, rather than the NextAuth.js default page.
 export const authConfig = {
+  providers: [],
   pages: {
     signIn: "/login",
   },
-};
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
+
+//The authorized callback is used to verify if the request is authorized to access a page via Next.js Middleware. It is called before a request is completed, and it receives an object with the auth and request properties. The auth property contains the user's session, and the request property contains the incoming request.
